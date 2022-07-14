734623722a7d446d63a4a503964c8f4eb82b66ed
Алексей <leksoner113@gmail.com>
2022-07-14 20:05:32 +0700

    практика с asinc await

--- Задача 5/index5.js
+++ Задача 5/index5.js
@@ -1,18 +1,48 @@
-new Promise(function (resolve) {
-  resolve(1);
-})
-  .then(function (result) {
-    return new Promise((resolve) => {
-      setTimeout(() => resolve(result * 2), 3000);
-      console.log(result);
-    });
-  })
-  .then(function (result) {
-    return new Promise((resolve) => {
-      setTimeout(() => resolve(result * 2), 3000);
-      console.log(result);
-    });
-  })
-  .then(function (result) {
-    alert(result);
+// new Promise(function (resolve) {
+//   resolve(1);
+// })
+//   .then(function (result) {
+//     return new Promise((resolve) => {
+//       setTimeout(() => resolve(result * 2), 3000);
+//       console.log(result);
+//     });
+//   })
+//   .then(function (result) {
+//     return new Promise((resolve) => {
+//       setTimeout(() => resolve(result * 2), 3000);
+//       console.log(result);
+//     });
+//   })
+//   .then(function (result) {
+//     alert(result);
+//   });
+
+// async function blabla() {
+//   let promise = Promise.resolve(1);
+//   let result = await promise;
+//   let newResult = new Promise((resolve) => {
+//     setTimeout(() => resolve(result * 2), 3000);
+//   });
+//   result = await newResult;
+//   let finalResult = new Promise((resolve) => {
+//     setTimeout(() => resolve(result * 2), 3000);
+//   });
+//   result = await finalResult;
+//   alert(result);
+// }
+// blabla();
+
+async function blabla() {
+  let result = await Promise.resolve(1);
+
+  result = await new Promise((resolve) => {
+    setTimeout(() => resolve(result * 2), 3000);
   });
+
+  result = await new Promise((resolve) => {
+    setTimeout(() => resolve(result * 2), 3000);
+  });
+  alert(result);
+  console.log(result);
+}
+blabla();
